b8bca5d9ae55da9bf999ff2e73ec06607035549b
JoãoDevel <joaovitormatosfontes15@gmail.com>
2025-11-13 22:07:27 +0000

    feat: add header and image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
+import Image from "next/image";
+import Banner from "../public/banner.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <main className="space-y-4 px-5">
+        <SearchInput />
+        <Image
+          src={Banner}
+          alt="Banner barber"
+          sizes="100vw"
+          className="h-full w-full"
+        />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
