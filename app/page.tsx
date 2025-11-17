bb685bfe4fdb9582a21b293807fae3a85878c05c
JoãoDevel <joaovitormatosfontes15@gmail.com>
2025-11-17 22:22:16 +0000

    feat:add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
 import Image from "next/image";
 import Banner from "../public/banner.png";
+import BookingItem from "@/components/BookingItem";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Header />
       <main className="space-y-4 px-5">
         <SearchInput />
@@ -15,8 +16,17 @@ const Home = () => {
           sizes="100vw"
           className="h-full w-full"
         />
+        <h2 className="text-foreground text-sm font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="corte de cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/f64f1bd4-59ce-4ee3-972d-2399937eeafc-16x.png"
+          date={new Date()}
+        />
       </main>
-    </div>
+    </main>
   );
 };
 
